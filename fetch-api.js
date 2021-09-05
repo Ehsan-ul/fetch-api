2488616c967c143605bc01ae9f71ee4143760ae7
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-09-05 07:51:08 +0600

    fetch api done

--- fetch-api.js
+++ fetch-api.js
@@ -6,20 +6,8 @@ document.getElementById('get-data').addEventListener('click', getData);
 
 function getData() {
   fetch('http://api.icndb.com/jokes/random')
-    .then((res) => {
-      console.log(res.json());
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      return data.value.joke;
-    })
-    .then((v) => {
-      console.log(v);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    // signle return e {} bracket deya lage nah
+    .then(res => res.json())
+    .then(data => { console.log(data); })
+    .catch(err => { console.log(err); });
 }
-
-
